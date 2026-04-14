1211ddedebcadd6eaa3c3a32d20c278ad7049039
Yoshrx <dawnbreak518@gmail.com>
2026-04-14 07:47:09 +0700

    ubah jadi 60 persen

--- database/migrations/2026_02_24_040656_add_foto_to_pengaduans_table.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Migration ini sudah digabung ke dalam create_pengaduans_table
-// Dibiarkan kosong agar tidak error saat migrate
-use Illuminate\Database\Migrations\Migration;
-return new class extends Migration {
-    public function up(): void {}
-    public function down(): void {}
-};
